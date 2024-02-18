f650d922fa68422b49826192a1ba07fa0dea0707
PTFOPlayer <patryk041103@gmail.com>
2024-02-18 19:58:41 +0100

    fix in vendored feature

--- build.rs
+++ build.rs
@@ -39,7 +39,10 @@ fn main() {
         .very_verbose(true)
         .build();
 
+    println!("cargo:warning={}", dst.display());
     println!("cargo:rustc-link-search=native={}/build", dst.display());
+    println!("cargo:rustc-link-search=native={}/lib", dst.display());
+    println!("cargo:rustc-link-search=native={}/lib64", dst.display());
     println!("cargo:rustc-link-lib=rocm_smi64");
     println!("cargo:rustc-link-lib=rsmi64");
 }
